41a9af441d208b9d39aeeac2e89068182f81bda6
Tamsir Rilley <tamsirrilley@gmail.com>
2024-11-19 18:52:40 -0500

    Fixed input types. Grabbed HTML elements from DOM. Added add-btn and it's container. Linked labels to inputs.

---
+++ script.js
@@ -0,0 +1,20 @@
+// DOM MANIPULATION
+
+// Input Elements from Tracker-Inputs
+
+// Input Buttons
+let incomeBtn = document.querySelector(".income-btn");
+let expenseBtn = document.querySelector(".expense-btn");
+let addBtn = document.querySelector(".add-btn");
+
+// Inputs
+let dateInput = document.querySelector(".date-input");
+let nameInput = document.querySelector(".name-input");
+let amountInput = document.querySelector(".amount-input");
+let descInput = document.querySelector(".desc-input");
+
+
+// Balance Card Number Element
+let balanceDisplay = document.querySelector(".balance-display");
+let totalIncomeDisplay = document.querySelector(".total-income-display");
+let totalExpensesDisplay = document.querySelector(".total-expenses-display");
